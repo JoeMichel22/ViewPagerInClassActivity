bd88baa5c26c5cfdcd8a4046726fb7f24bef5061
Joe Michel <tun69277@temple.edu>
2023-04-04 12:30:59 -0400

    Final changes

--- app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
+++ app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
@@ -2,15 +2,24 @@ package edu.temple.viewpagerinclasssctivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
+
+    val pager: ViewPager2 by lazy {
+        findViewById(R.id.pageViewer)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val pager: ViewPager2 by lazy {
-            findViewById(R.id.pageViewer)
+        pager.adapter= object: FragmentStateAdapter(this){
+            override fun getItemCount()= 10
+
+            override fun createFragment(position: Int)= TextFragment.newInstance((position + 1).toString())
         }
+
     }
 }
