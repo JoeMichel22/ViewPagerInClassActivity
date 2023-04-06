4c12891f6b0c105606e068070fb4fd5b86e8983d
Joe Michel <tun69277@temple.edu>
2023-04-06 12:15:04 -0400

    Added page jump functionality

--- app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
+++ app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
@@ -2,21 +2,37 @@ package edu.temple.viewpagerinclasssctivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
 
-    val pager: ViewPager2 by lazy {
+    //ctrl+B opens the functionality definition of functions and componenets in andriod studio
+    //supportFragmentManager.findFragmentByTag("f" + position of fragment) needed for assignment 8
+
+    private var numberOfPages= 0
+    private val btnAdd: Button by lazy{
+        findViewById(R.id.btnAdd)
+    }
+    private val pager: ViewPager2 by lazy {
         findViewById(R.id.pageViewer)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnAdd.setOnClickListener {
+            numberOfPages ++
+            //pager.adapter?.notifyDataSetChanged() //used to let adapter know that data has been changed outside of it
+            pager.adapter?.notifyItemInserted(numberOfPages - 1)
+
+            pager.currentItem= numberOfPages-1
+        }
+
         pager.adapter= object: FragmentStateAdapter(this){
-            override fun getItemCount()= 10
+            override fun getItemCount()= numberOfPages
 
             override fun createFragment(position: Int)= TextFragment.newInstance((position + 1).toString())
         }
